91b9420251fa8dec48f6ddb5c433e2bb69820207
kadukitesesi <kadukitesesi@gmail.com>
2024-06-20 20:13:30 -0300

    Mais estudos de spring security

--- src/main/java/com/kadukitesesi/seguranca/entity/User.java
+++ src/main/java/com/kadukitesesi/seguranca/entity/User.java
@@ -7,6 +7,8 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Entity(name = "User")
 @Table(name = "users")
 @NoArgsConstructor
@@ -23,4 +25,10 @@ public class User {
     private String email;
 
     private String senha;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.PERSIST)
+    @JoinTable(name = "users_roles",
+    joinColumns = @JoinColumn(name = "user_id"),
+    inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private List<Role> roles;
 }
